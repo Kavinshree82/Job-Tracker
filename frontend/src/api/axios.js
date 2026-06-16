679df26733a29e55953004d15694c90309333bdb
kavinshree82 <kavinshree82@gmail.com>
2026-06-16 12:15:01 +0530

    Update API URL to production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://job-tracker-api-xga3.onrender.com/api',
 });
 
 export default instance;
